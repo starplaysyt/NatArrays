ce01e2e68b81f95a6eb9798366cce0cd78084a75
starplaysyt <horaudmitry@gmail.com>
2025-12-13 16:57:10 +0300

    Wrapper for Unit2 implemented - Point2. Just contains Unit2 for semantics help. - I wasted all day trying to make this the most efficient shit ever. - Also some contracts and cross-compability work done. - BENCHMARK IN XUNIT!! YESSSS! (i'm dumb as shit)

---
+++ NatLib.Core/Point2.cs
@@ -0,0 +1,96 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace NatLib.Core;
+
+public struct Point2<T> : IEquatable<Point2<T>> where T : INumber<T>
+{
+    private Unit2<T> _value;
+    
+    // Container properties contraction
+    public T X { get => _value.X; set => _value.X = value; }
+    public T Y { get => _value.Y; set => _value.Y = value; }
+    
+    // Constructors
+    public Point2() : this(T.Zero, T.Zero) { }
+    
+    public Point2(T x, T y) { _value = new Unit2<T>(x, y); }
+    
+    public Point2(Unit2<T> u) { _value = u; }
+    
+    // Conversion operators
+    public static implicit operator Unit2<T>(Point2<T> point) => point._value;
+    
+    public static implicit operator Point2<T>(Unit2<T> unit2) => new(unit2);
+    
+    // Default operators
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2<T> operator+(Point2<T> u1, Point2<T> u2) => 
+        new(u1._value.X + u2._value.X, u1._value.Y + u2._value.Y);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2<T> operator-(Point2<T> u1, Point2<T> u2)
+        => new(u1._value.X - u2._value.X, u1._value.Y - u2._value.Y);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2<T> operator*(Point2<T> u1, Point2<T> u2)
+        => new(u1._value.X * u2._value.X, u1._value.Y * u2._value.Y);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2<T> operator/(Point2<T> u1, Point2<T> u2)
+        => new(u1._value.X / u2._value.X, u1._value.Y / u2._value.Y);
+    
+    // Operators with T
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2<T> operator+(Point2<T> u1, T u2)
+        => new(u1._value.X + u2, u1._value.Y + u2);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2<T> operator-(Point2<T> u1, T u2)
+        => new(u1._value.X - u2, u1._value.Y - u2);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2<T> operator*(Point2<T> u1, T u2)
+        => new(u1._value.X * u2, u1._value.Y * u2);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2<T> operator/(Point2<T> u1, T u2)
+        => new(u1._value.X / u2, u1._value.Y / u2);
+
+    // Compare operators 
+    public static bool operator ==(Point2<T> left, Point2<T> right) => left.Equals(right);
+
+    public static bool operator !=(Point2<T> left, Point2<T> right) => !(left == right);
+    
+    // Operations with existing struct
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Add(T value) { _value.Add(value); }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Subtract(T value) { _value.Subtract(value); }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Multiply(T value) { _value.Multiply(value); }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Divide(T value) { _value.Divide(value); }
+    
+    // Overrides
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => _value.ToString();
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(Point2<T> other) => _value.Equals(other._value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object? obj) => obj is Point2<T> other && Equals(other);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode() => _value.GetHashCode();
+    
+    // Deconstructor
+    public void Deconstruct(out T x, out T y) => (x, y) = (X, Y);
+    
+    // Type-specific operations
+    
+}
--- NatLib.Tests/UnitTest1.cs
+++ NatLib.Tests/UnitTest1.cs
@@ -1,9 +1,71 @@
-﻿namespace NatLib.Tests;
+﻿using System.Diagnostics;
+using NatLib.Core;
+using Xunit.Abstractions;
+
+namespace NatLib.Tests;
 
 public class UnitTest1
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public UnitTest1(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    private const int Iterations = 10_000_000;
+    
     [Fact]
     public void Test1()
-    {
+    {  
+        var u1 = new Unit2<float>(1.0f, 2.0f);
+        var u2 = new Unit2<float>(3.0f, 4.0f);
+
+        var p1 = new Point2<float>(1.0f, 2.0f);
+        var p2 = new Point2<float>(3.0f, 4.0f);
+
+        // JIT warmup
+        for (int i = 0; i < 100_000; i++)
+        {
+            _ = u1 + u2;
+            _ = p1 + p2;
+        }
+
+        // -------------------------
+        // Unit2 benchmark
+        // -------------------------
+        var swUnit = Stopwatch.StartNew();
+
+        Unit2<float> unitResult = default;
+        for (int i = 0; i < Iterations; i++)
+        {
+            unitResult = u1 + u2;
+        }
+
+        swUnit.Stop();
+
+        // -------------------------
+        // Point2 benchmark
+        // -------------------------
+        var swPoint = Stopwatch.StartNew();
+
+        Point2<float> pointResult = default;
+        for (int i = 0; i < Iterations; i++)
+        {
+            pointResult = p1 + p2;
+        }
+
+        swPoint.Stop();
+
+        // Prevent dead-code elimination
+        Assert.NotEqual(float.NaN, unitResult.X);
+        Assert.NotEqual(float.NaN, pointResult.X);
+
+        // Output results
+        var unitTime = swUnit.Elapsed.TotalMilliseconds;
+        var pointTime = swPoint.Elapsed.TotalMilliseconds;
+
+        _testOutputHelper.WriteLine($"Unit2:  {unitTime:F2} ms");
+        _testOutputHelper.WriteLine($"Point2: {pointTime:F2} ms");
     }
 }
