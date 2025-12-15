1eefd03aefb49e20ad6c050f19de93cb7058f6f4
starplaysyt <horaudmitry@gmail.com>
2025-12-15 17:44:35 +0300

    Vector2 released, currently not tested

--- NatLib.Core/Vector2.cs
+++ NatLib.Core/Vector2.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace NatLib.Core;
 
-public struct Point2<T> : IEquatable<Point2<T>> where T : INumber<T>
+public struct Vector2<T> : IEquatable<Vector2<T>> where T : INumber<T>, IRootFunctions<T>
 {
     private Unit2<T> _value;
     
@@ -12,55 +12,55 @@ public struct Point2<T> : IEquatable<Point2<T>> where T : INumber<T>
     public T Y { get => _value.Y; set => _value.Y = value; }
     
     // Constructors
-    public Point2() : this(T.Zero, T.Zero) { }
+    public Vector2() : this(T.Zero, T.Zero) { }
     
-    public Point2(T x, T y) { _value = new Unit2<T>(x, y); }
+    public Vector2(T x, T y) { _value = new Unit2<T>(x, y); }
     
-    public Point2(Unit2<T> u) { _value = u; }
+    public Vector2(Unit2<T> u) { _value = u; }
     
     // Conversion operators
-    public static implicit operator Unit2<T>(Point2<T> point) => point._value;
+    public static implicit operator Unit2<T>(Vector2<T> point) => point._value;
     
-    public static implicit operator Point2<T>(Unit2<T> unit2) => new(unit2);
+    public static implicit operator Vector2<T>(Unit2<T> unit2) => new(unit2);
     
     // Default operators
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Point2<T> operator+(Point2<T> u1, Point2<T> u2) =>   
+    public static Vector2<T> operator+(Vector2<T> u1, Vector2<T> u2) =>   
         new(u1._value.X + u2._value.X, u1._value.Y + u2._value.Y);
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Point2<T> operator-(Point2<T> u1, Point2<T> u2)
+    public static Vector2<T> operator-(Vector2<T> u1, Vector2<T> u2)
         => new(u1._value.X - u2._value.X, u1._value.Y - u2._value.Y);
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Point2<T> operator*(Point2<T> u1, Point2<T> u2)
+    public static Vector2<T> operator*(Vector2<T> u1, Vector2<T> u2)
         => new(u1._value.X * u2._value.X, u1._value.Y * u2._value.Y);
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Point2<T> operator/(Point2<T> u1, Point2<T> u2)
+    public static Vector2<T> operator/(Vector2<T> u1, Vector2<T> u2)
         => new(u1._value.X / u2._value.X, u1._value.Y / u2._value.Y);
     
     // Operators with T
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Point2<T> operator+(Point2<T> u1, T u2)
+    public static Vector2<T> operator+(Vector2<T> u1, T u2)
         => new(u1._value.X + u2, u1._value.Y + u2);
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Point2<T> operator-(Point2<T> u1, T u2)
+    public static Vector2<T> operator-(Vector2<T> u1, T u2)
         => new(u1._value.X - u2, u1._value.Y - u2);
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Point2<T> operator*(Point2<T> u1, T u2)
+    public static Vector2<T> operator*(Vector2<T> u1, T u2)
         => new(u1._value.X * u2, u1._value.Y * u2);
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Point2<T> operator/(Point2<T> u1, T u2)
+    public static Vector2<T> operator/(Vector2<T> u1, T u2)
         => new(u1._value.X / u2, u1._value.Y / u2);
 
     // Compare operators 
-    public static bool operator ==(Point2<T> left, Point2<T> right) => left.Equals(right);
+    public static bool operator ==(Vector2<T> left, Vector2<T> right) => left.Equals(right);
 
-    public static bool operator !=(Point2<T> left, Point2<T> right) => !(left == right);
+    public static bool operator !=(Vector2<T> left, Vector2<T> right) => !(left == right);
     
     // Operations with existing struct
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -80,10 +80,10 @@ public struct Point2<T> : IEquatable<Point2<T>> where T : INumber<T>
     public override string ToString() => _value.ToString();
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool Equals(Point2<T> other) => _value.Equals(other._value);
+    public bool Equals(Vector2<T> other) => _value.Equals(other._value);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public override bool Equals(object? obj) => obj is Point2<T> other && Equals(other);
+    public override bool Equals(object? obj) => obj is Vector2<T> other && Equals(other);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public override int GetHashCode() => _value.GetHashCode();
@@ -93,4 +93,12 @@ public struct Point2<T> : IEquatable<Point2<T>> where T : INumber<T>
     
     // Type-specific operations
     
+    public static Vector2<T> Zero => default;
+
+    public T LengthSquared => _value.X * _value.X + _value.Y * _value.Y;
+
+    public T Length => T.Sqrt(LengthSquared);
+    
+    public Vector2<T> Normalized() => this * (T.One / Length);
+
 }
