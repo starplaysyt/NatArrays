4f29f7a0bada4c71effde545d820f110d882530f
starplaysyt <horaudmitry@gmail.com>
2025-12-16 11:22:03 +0300

    Vector2 prod-ready.

--- NatLib.Core/Vector2.cs
+++ NatLib.Core/Vector2.cs
@@ -3,13 +3,17 @@ using System.Runtime.CompilerServices;
 
 namespace NatLib.Core;
 
-public struct Vector2<T> : IEquatable<Vector2<T>> where T : INumber<T>, IRootFunctions<T>
+public struct Vector2<T> : IEquatable<Vector2<T>> where T : INumber<T>
 {
     private Unit2<T> _value;
     
     // Container properties contraction
     public T X { get => _value.X; set => _value.X = value; }
     public T Y { get => _value.Y; set => _value.Y = value; }
+    
+    // Zero and one static properties
+    public static readonly Vector2<T> Zero = new (T.Zero, T.Zero);
+    public static readonly Vector2<T> One = new (T.One, T.One);
     
     // Constructors
     public Vector2() : this(T.Zero, T.Zero) { }
@@ -24,36 +28,28 @@ public struct Vector2<T> : IEquatable<Vector2<T>> where T : INumber<T>, IRootFun
     public static implicit operator Vector2<T>(Unit2<T> unit2) => new(unit2);
     
     // Default operators
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2<T> operator+(Vector2<T> u1, Vector2<T> u2) =>   
         new(u1._value.X + u2._value.X, u1._value.Y + u2._value.Y);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2<T> operator-(Vector2<T> u1, Vector2<T> u2)
         => new(u1._value.X - u2._value.X, u1._value.Y - u2._value.Y);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2<T> operator*(Vector2<T> u1, Vector2<T> u2)
         => new(u1._value.X * u2._value.X, u1._value.Y * u2._value.Y);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2<T> operator/(Vector2<T> u1, Vector2<T> u2)
         => new(u1._value.X / u2._value.X, u1._value.Y / u2._value.Y);
     
     // Operators with T
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2<T> operator+(Vector2<T> u1, T u2)
         => new(u1._value.X + u2, u1._value.Y + u2);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2<T> operator-(Vector2<T> u1, T u2)
         => new(u1._value.X - u2, u1._value.Y - u2);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2<T> operator*(Vector2<T> u1, T u2)
         => new(u1._value.X * u2, u1._value.Y * u2);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2<T> operator/(Vector2<T> u1, T u2)
         => new(u1._value.X / u2, u1._value.Y / u2);
 
@@ -63,42 +59,20 @@ public struct Vector2<T> : IEquatable<Vector2<T>> where T : INumber<T>, IRootFun
     public static bool operator !=(Vector2<T> left, Vector2<T> right) => !(left == right);
     
     // Operations with existing struct
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Add(T value) { _value.Add(value); }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Subtract(T value) { _value.Subtract(value); }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Multiply(T value) { _value.Multiply(value); }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Divide(T value) { _value.Divide(value); }
+    public static void Add(ref Vector2<T> left, T value) { Unit2<T>.Add(ref left._value, value); }
+    public static void Subtract(ref Vector2<T> left, T value) { Unit2<T>.Subtract(ref left._value, value); }
+    public static void Multiply(ref Vector2<T> left, T value) { Unit2<T>.Multiply(ref left._value, value); }
+    public static void Divide(ref Vector2<T> left, T value) { Unit2<T>.Divide(ref left._value, value); }
     
     // Overrides
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public override string ToString() => _value.ToString();
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool Equals(Vector2<T> other) => _value.Equals(other._value);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     public override bool Equals(object? obj) => obj is Vector2<T> other && Equals(other);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     public override int GetHashCode() => _value.GetHashCode();
     
     // Deconstructor
     public void Deconstruct(out T x, out T y) => (x, y) = (X, Y);
-    
-    // Type-specific operations
-    
-    public static Vector2<T> Zero => default;
-
-    public T LengthSquared => _value.X * _value.X + _value.Y * _value.Y;
-
-    public T Length => T.Sqrt(LengthSquared);
-    
-    public Vector2<T> Normalized() => this * (T.One / Length);
-
 }
