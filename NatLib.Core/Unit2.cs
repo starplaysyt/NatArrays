d1a2bbef06b8bed1ca7134f2650464b09f674f81
starplaysyt <horaudmitry@gmail.com>
2025-12-16 11:21:47 +0300

    Unit2 prod-ready.

--- NatLib.Core/Unit2.cs
+++ NatLib.Core/Unit2.cs
@@ -10,12 +10,20 @@ namespace NatLib.Core;
 /// <typeparam name="T">Type of each variable</typeparam>
 public struct Unit2<T> : IEquatable<Unit2<T>> where T : INumber<T>
 {
+    // Container properties
     public T X;
     public T Y;
-
+    
+    // Zero and one static properties
+    public static readonly Vector2<T> Zero = new (T.Zero, T.Zero);
+    public static readonly Vector2<T> One = new (T.One, T.One);
+    
+    // Constructors
     public Unit2(T x, T y) { X = x; Y = y; }
 
     public Unit2() : this(T.Zero, T.Zero) { }
+    
+    // Default operators
 
     public static Unit2<T> operator+(Unit2<T> u1, Unit2<T> u2)
         => new(u1.X + u2.X, u1.Y + u2.Y);
@@ -29,6 +37,7 @@ public struct Unit2<T> : IEquatable<Unit2<T>> where T : INumber<T>
     public static Unit2<T> operator/(Unit2<T> u1, Unit2<T> u2)
         => new(u1.X / u2.X, u1.Y / u2.Y);
     
+    // Operators with T
     public static Unit2<T> operator+(Unit2<T> u1, T u2)
         => new(u1.X + u2, u1.Y + u2);
     
@@ -41,36 +50,25 @@ public struct Unit2<T> : IEquatable<Unit2<T>> where T : INumber<T>
     public static Unit2<T> operator/(Unit2<T> u1, T u2)
         => new(u1.X / u2, u1.Y / u2);
     
+    // Compare operators 
     public static bool operator ==(Unit2<T> left, Unit2<T> right) =>
         left.Equals(right);
 
     public static bool operator !=(Unit2<T> left, Unit2<T> right) => !(left == right);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Add(T value) { X += value; Y += value; }
+    // Operations with existing struct
+    public static void Add(ref Unit2<T> left, T value) { left.X += value; left.Y += value; }
+    public static void Subtract(ref Unit2<T> left, T value) { left.X -= value; left.Y -= value; }
+    public static void Multiply(ref Unit2<T> left, T value) { left.X *= value; left.Y *= value; }
+    public static void Divide(ref Unit2<T> left, T value) { left.X /= value; left.Y /= value; }
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Subtract(T value) { X -= value; Y -= value; }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Multiply(T value) { X *= value; Y *= value; }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Divide(T value) { X /= value; Y /= value; }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    // Overrides
     public override string ToString() => $"({X}, {Y})";
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public override bool Equals([NotNullWhen(true)] object? obj) => 
         obj is Unit2<T> unit2 && Equals(unit2);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool Equals(Unit2<T> other) => X.Equals(other.X) && Y.Equals(other.Y);
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public override int GetHashCode() => HashCode.Combine(X, Y);
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Deconstruct(out T x, out T y) => (x, y) = (X, Y);
 }
