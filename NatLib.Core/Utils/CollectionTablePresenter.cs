9ec9e89437b4eb080a3b3b419576cf7d249aeed3
starplaysyt <horaudmitry@gmail.com>
2026-03-12 02:14:26 +0300

    CollectionTablePresenter changed. - removed generic limitation for now, need to implement something smart to hangle. - removed debug information - deprecated BuildTableNonNumerical function, not usable and will never be.

--- NatLib.Core/Utils/CollectionTablePresenter.cs
+++ NatLib.Core/Utils/CollectionTablePresenter.cs
@@ -6,7 +6,8 @@ using NatLib.Core.Unification;
 
 namespace NatLib.Core.Utils;
 
-public class CollectionTablePresenter<T> where T : class
+// TODO: Deal with generic limitation, causes crash when there is nothing to reflect
+public class CollectionTablePresenter<T>
 {
     private readonly PropertyInfo[] _properties;
     private int[] _lengths;
@@ -48,12 +49,8 @@ public class CollectionTablePresenter<T> where T : class
         var numbersLength = ShowNumbers ? Collection.Count.ToString().Length : 0;
         var configuration = Configuration;
         _lengths = GetMaxLengthArray(Collection.AsEnumerable(), _properties, numbersLength);
-        
-        Console.WriteLine(string.Join('\n', _lengths.Zip(Collection).Select((tuple) => $"{tuple.First} : {tuple.Second}")));
 
         var lineLength = 1 + _lengths.Sum(l => l + 3);
-
-        Console.WriteLine("LineLength: " + lineLength);
         
         var headerTopDivider = StringUtils.GenerateJoin(
             configuration.CornerTopLeft, 
@@ -109,13 +106,6 @@ public class CollectionTablePresenter<T> where T : class
         StringBuilder bld = new(
             (lineLength + 1) * (4 + dataStrings.Length) - 1);
         
-        // if (headerTopDivider.Length != lineLength 
-        //     || header.Length != lineLength
-        //     || headerBottomDivider.Length != lineLength
-        //     || dataStrings.Select(ds => ds.Length != lineLength).All(b => b )
-        //     || footerDivider.Length != lineLength)
-        //     Console.WriteLine("Exception in lines!");
-        
         bld.AppendLine(headerTopDivider);
         bld.AppendLine(header);
         bld.AppendLine(headerBottomDivider);
@@ -124,82 +114,7 @@ public class CollectionTablePresenter<T> where T : class
         bld.Append(footerDivider);
         _tableContainer = bld.ToString();
         
-        var stringBuilderLengthCalc = (lineLength + 1) * (4 + dataStrings.Length) - 1;
-        var stringBuilderLengthAct = bld.Length;
-        if (stringBuilderLengthAct != stringBuilderLengthCalc)
-            Console.WriteLine($"Result and planting mismatch! Act: {stringBuilderLengthAct} Calc: {stringBuilderLengthCalc}");
-        
         return _tableContainer;
-    }
-
-    private string BuildTableNonNumerical()
-    {
-        var configuration = Configuration;
-
-        _lengths = GetMaxLengthArray(Collection.AsEnumerable(), _properties);
-
-        var lineLength = 1 + _lengths.Sum(l => l + 3);
-        
-        var headerTopDivider = StringUtils.GenerateJoin(
-            configuration.CornerTopLeft, 
-            configuration.CornerTopRight, 
-            configuration.HorizontalLine, 
-            configuration.SectionTBottom, 
-            _lengths);
-        
-        var header = string.Create( // generating headers
-                lineLength,
-                (
-                    ReflectionUtils.GetPropertiesNames<T>(_properties).ToArray(),
-                    _lengths,
-                    configuration.VerticalLine,
-                    Alignment.Begin
-                ),
-            SpanCharUtils.WrapJoinSpan
-            );
-
-        var headerBottomDivider = StringUtils.GenerateJoin(
-            configuration.SectionTRight,
-            configuration.SectionTLeft,
-            configuration.HorizontalLine,
-            configuration.SectionX,
-            _lengths);
-
-        var dataStrings = Collection
-            .AsParallel()
-            .Select(item => string.Create( // generating data strings
-                lineLength,
-                (
-                    ReflectionUtils.GetPropertiesToString(_properties, item).ToArray(),
-                    _lengths,
-                    configuration.VerticalLine,
-                    Alignment.Begin
-                ),
-                SpanCharUtils.WrapJoinSpan)
-            ).ToArray();
-        
-        var footerDivider = StringUtils.GenerateJoin(
-            configuration.CornerBottomLeft,
-            configuration.CornerBottomRight,
-            configuration.HorizontalLine,
-            configuration.SectionTTop,
-            _lengths);
-
-        StringBuilder bld = new(
-            headerBottomDivider.Length + 
-            header.Length + 
-            headerBottomDivider.Length + 
-            dataStrings.Sum(t => t.Length) + 
-            footerDivider.Length +
-            5 + dataStrings.Length);
-        
-        bld.AppendLine(headerTopDivider);
-        bld.AppendLine(header);
-        bld.AppendLine(headerBottomDivider);
-        bld.AppendJoin('\n', dataStrings);
-        bld.Append('\n');
-        bld.AppendLine(footerDivider);
-        return bld.ToString();
     }
 
     private int[] GetMaxLengthArray(IEnumerable<T> collection, PropertyInfo[] properties, int numericsLength = 0)
